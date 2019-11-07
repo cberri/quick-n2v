d635b104104e342e1727a258c6866aec8fe3f636
aiporre <ariel.iporre.rivas@gmail.com>
2019-11-07 20:11:35 +0100

    enforce channels selection with input dims. enforce png output format. Optional automatic detection of dims of image

--- onimages.py
+++ onimages.py
@@ -39,31 +39,40 @@ def denoise_images(images_path, output_path):
     basedir = 'models'
     model = N2V(config=None, name=model_name, basedir=basedir)
     warning_print = False
+    print('DENOISING......')
     for f in tqdm(os.listdir(images_path)):
-        if os.path.isfile(os.path.join(images_path,f)) and f.endswith('.tif'):
+        if os.path.isfile(os.path.join(images_path,f)) and f.endswith(args.fileName.replace('*','')):
             img = imread(os.path.join(images_path,f))
-            if len(img.shape)==2:
+
+            # if args.dims = N means Not Defined. Then the program decides which is the one.            
+            if len(img.shape)==2 and args.dims=='N':
                 axes = 'YX'
-            elif len(img.shape)==3:
+            elif len(img.shape)==3 and args.dims=='N':
                 axes = 'YXC'
             else:
-                print('Invalid format image: ', img.shape, 'formats supported YX and YXC')
-                break
+                axes = args.dims
+            # Ensures that the input has 3 channels if read image axes are XYC or YXC
             if 'C' in axes and img.shape[-1]==4:
                 if not warning_print:
                     print('Warning: alpha channels will be removed from all images.')
                     warning_print = True
                     print('Org. shape: ', img.shape, 'New shape:', img[...,:3].shape)
                 img = img[...,:3]
+            if len(axes) == 2 and not len(img.shape)==2:
+                if not warning_print:
+                    print('Warning: Enfoced 2 channels. 3 channels detected. All images will be converted.')
+                    warning_print = True
+                    print('Org. shape: ', img.shape, 'New shape:', img[...,0].shape)
+                img = img[...,0] # taking the red channel
             pred = model.predict(img, axes=axes)
-            f_out = os.path.join(output_path, f.replace(args.fileName))
+            f_out = os.path.join(output_path, f.replace(args.fileName.replace('*',''),'.png'))
             print('pred.max(): ', pred.max(), 'pred.min()', pred.min())
             print('saving file: ', f_out)
             imsave(f_out, clip(pred, 0.0, 1.0), cmap='gray')
 
 # Creating the path of denoised images
 output_path = create_output_directory(args.output)
-
+print('Output path is: ', output_path )
 if args.train=='y' or not os.path.exists('models/N2V/weights_best.h5'):
     training_args = generate_args(data_path=args.target, fileName=args.fileName, dims=args.dims)
     model, X, X_val = prepare_training_data(training_args)
